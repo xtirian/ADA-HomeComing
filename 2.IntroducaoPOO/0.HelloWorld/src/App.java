91397374f5f67fcce410feefcabaaae1683ccff4
xTirian <tirianx@yahoo.com>
2023-11-08 21:17:18 -0300

    add primeiro hello world ao java

---
+++ 2.IntroducaoPOO/0.HelloWorld/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        //VAR ARGS significa que poderemos colocar uma quantidade indefinida de 
+        System.out.println("Hello, World!");
+
+        byte varByte = 120; //byte vai do -128 ao 127
+        System.out.println(varByte);
+        short varShort; // -32768 a 32767        
+        char varChar; //0 a 65535
+        int varInt; // -2bi a 2bi
+        long varLong; // -9t a 9t
+
+        varInt = 214783;
+
+        System.out.println(varInt);
+    }
+}
